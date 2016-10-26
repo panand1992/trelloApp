f7002e959978446e53294ba3a2a6b42f14f1562d
Prashant Anand <panandiitk@gmail.com>
2016-10-26 05:39:27 +0530

    app structure added

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var app = angular.module('trelloApp', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "/templates/home.html"
+    });
+});
